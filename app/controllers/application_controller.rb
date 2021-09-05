0beb377f939da40ebeddcc0f00b1d49e6ae2b5ff
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-09-05 20:49:30 +0900

    ユーザー画像機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :login_required
 
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :icon])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :icon])
+  end
+
   private
   def login_required
     redirect_to new_session_path unless current_user
