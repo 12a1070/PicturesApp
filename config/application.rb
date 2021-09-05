d01a7b2bd0b170c5fb4eb03a78efb363ff46fd9c
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-09-05 17:42:05 +0900

    init

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +12,9 @@ module Syasin
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+config.time_zone = 'Tokyo'
+config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
