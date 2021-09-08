c715c38ea3078ace6b0daae5272fc3218809a3e9
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-09-08 19:08:48 +0900

    お気に入りidエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
 
   root 'users#new'
+  resources :posts
   resources :contacts
   resources :pictures
   resources :sessions, only: [:new, :create, :destroy]
