0d487996a1f347f1b13ede86fbe98e63a3d5f0cc
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-09-09 18:28:19 +0900

    “mailer実装”

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -26,6 +26,7 @@ class PicturesController < ApplicationController
     @picture = current_user.pictures.build(picture_params)
     respond_to do |format|
       if @picture.save
+        ContactMailer.contact_mail(@picture).deliver
         format.html { redirect_to @picture, notice: "Picture was successfully created." }
         format.json { render :show, status: :created, location: @picture }
       else
